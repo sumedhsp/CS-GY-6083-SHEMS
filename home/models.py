3e5d2dd25f4364c56774ee876f3d1f10263b326f
Sumedh Parvatikar <sumedhsparvatikar@gmail.com>
2023-12-19 10:03:06 -0500

    CN2: Updated the schema

--- home/models.py
+++ home/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
 class Address(models.Model):
     address_id = models.AutoField(primary_key=True)
     unit_number = models.CharField(max_length=50)
     address_line = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
     zipcode = models.IntegerField()
 
     class Meta:
@@ -16,13 +17,13 @@ class Address(models.Model):
 
 class Customers(models.Model):
     customer_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=100)
+    phone_num = models.CharField(max_length=10)
+    dob = models.DateField()
+    first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     billing_address = models.ForeignKey(Address, models.DO_NOTHING)
-
-    #additional fields
-    email = models.CharField(max_length=50, null=True)
-    registered_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         managed = False
@@ -33,9 +34,9 @@ class Device(models.Model):
     model_id = models.CharField(primary_key=True, max_length=100)
     device_type = models.CharField(max_length=100)
     model_number = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100, blank=True, null=True)
-    release_year = models.IntegerField(blank=True, null=True)
-    manufacture_year = models.IntegerField(blank=True, null=True)
+    brand = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    manufacture_year = models.IntegerField()
 
     class Meta:
         managed = False
@@ -45,7 +46,7 @@ class Device(models.Model):
 class Devicedata(models.Model):
     data_id = models.AutoField(primary_key=True)
     enrolled_device = models.ForeignKey('Enrolleddevices', models.DO_NOTHING)
-    event_timestamp = models.DateTimeField(blank=True, null=True)
+    event_timestamp = models.DateTimeField()
     event_label = models.CharField(max_length=100)
     event_value = models.DecimalField(max_digits=18, decimal_places=6, blank=True, null=True)
 
@@ -54,14 +55,14 @@ class Devicedata(models.Model):
         db_table = 'devicedata'
 
 
-class Energyprices(models.Model):
+class EnergyPrices(models.Model):
     zipcode = models.IntegerField(primary_key=True)  # The composite primary key (zipcode, price_timestamp) found, that is not supported. The first column is selected.
     price_timestamp = models.DateTimeField()
     price_per_kwh = models.DecimalField(max_digits=18, decimal_places=6)
 
     class Meta:
         managed = False
-        db_table = 'energyprices'
+        db_table = 'energy_prices'
         unique_together = (('zipcode', 'price_timestamp'),)
 
 
@@ -79,11 +80,11 @@ class Servicelocations(models.Model):
     sl_id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customers, models.DO_NOTHING, blank=True, null=True)
     address = models.ForeignKey(Address, models.DO_NOTHING, blank=True, null=True)
-    start_date = models.DateField(blank=True, null=True)
-    apt_area = models.DecimalField(max_digits=18, decimal_places=6, blank=True, null=True)
-    num_bedrooms = models.IntegerField(blank=True, null=True)
-    num_occupants = models.IntegerField(blank=True, null=True)
+    service_location_label = models.CharField(max_length=100)
+    start_date = models.DateField()
+    apt_area = models.DecimalField(max_digits=18, decimal_places=6)
+    num_bedrooms = models.IntegerField()
+    num_occupants = models.IntegerField()
 
     class Meta:
-        managed = False
-        db_table = 'servicelocations'
+        managed = Falsedb_table = 'servicelocations'
